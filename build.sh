547df61707bb429e80a0e7b7a8e6788b74db4b58
Ray Young <ray.arden.young@gmail.com>
2022-11-29 14:50:58 -0500

    build.sh adjustment

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 pipenv install
 
-pipenv run python manage.py migrate 0027_alter_favorite_card_alter_favorite_user
+pipenv run python manage.py migrate ecard 0027_alter_favorite_card_alter_favorite_user
 pipenv run python manage.py collectstatic --no-input
 pipenv run python manage.py createsuperuser
